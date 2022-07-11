ff3501dede097f7f49a53aee93523d5da6520f29
hsinha44 <hsinha44@gmail.com>
2022-07-11 17:46:19 +1000

    Added tether, token and stakingDapp contracts.

---
+++ src/contracts/Token.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Token{
+
+    string public name = "dummy token";
+    string public symbol = "DUM";
+    uint public totalSupply = 1000000000000000000000000; //1 million (plus 18 zeros)
+    uint public decimal = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint) public balance;
+
+    //track the approwed allowance of a spender for every owner
+    mapping(address => mapping(address => uint)) public allowance;
+
+    constructor(){
+        balance[msg.sender] = totalSupply; //all the tokens are sent to the creator of SC
+    }
+
+    function transfer(address _to, uint _value) public returns(bool) {
+          require(balance[msg.sender] >= _value, "Insufficient balance.");
+          balance[msg.sender] -= _value;
+          balance[_to] += _value;
+
+          emit Transfer(msg.sender, _to, _value);
+          return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns(bool) {
+          require(balance[_from] >= _value, "Insufficient balance.");
+          balance[_from] -= _value;
+          balance[_to] += _value;
+
+          emit Transfer(_from, _to, _value);
+          return true;
+    }
+
+}
---
+++ src/contracts/stakingDapp.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./Token.sol";
+import "./Tether.sol";
+
+contract Staking_Dapp{
+    string public name = "Staking Dapp";
+    address public owner;
+
+    Token public dummy_token;
+    Tether public tether_token;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
+    constructor(Token _dummyToken, Tether _tetherToken) {
+        dummy_token = _dummyToken;
+        tether_token = _tetherToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint _amount) public {
+        require(_amount > 0, "Staking amount must be greater than 0.");
+
+        tether_token.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] += _amount;
+
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function unstakingTokens() public {
+        uint balance = stakingBalance[msg.sender];
+        require(balance > 0, "No staked tokens.");
+
+        stakingBalance[msg.sender] = 0;
+        isStaked[msg.sender] = false;
+        tether_token.transfer(msg.sender, balance);
+        
+    }
+
+    function issueToken() public{
+        require(msg.sender == owner, "Only the owner can issue tokens");
+
+        for (uint i = 0; i < stakers.length; i++){
+            address recipient = stakers[i];
+            uint recipientBalance = stakingBalance[recipient];
+
+            if (recipientBalance > 0) {
+                dummy_token.transfer(recipient, recipientBalance);
+            }
+        }
+    }
+
+}
